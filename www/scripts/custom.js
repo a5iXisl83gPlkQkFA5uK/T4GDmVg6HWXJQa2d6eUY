e1ed055b1b5b3df38aa700c559fe60012a47e26e
honsoworld <taylor@honsoworld.com>
2013-11-15 00:04:49 -0500

    working

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -55,23 +55,59 @@ $(document).ready(function(){
 		document.ontouchmove = function(event){ event.allowDefault();}
 	});
 	
+	$('#cal-357').click(function(){ badger.fetch("357", 47909); });
+	$('#cal-380').click(function(){ badger.fetch("380", 47909); });
+	$('#cal-40').click(function(){ badger.fetch("40", 47909); });
+	$('#cal-44').click(function(){ badger.fetch("44", 47909); });
+	$('#cal-45').click(function(){ badger.fetch("45", 47909); });
+	$('#cal-9').click(function(){ badger.fetch("9", 47909); });
+	$('#cal-12').click(function(){ badger.fetch("12", 47909); });
+	$('#cal-20').click(function(){ badger.fetch("20", 47909); });
+	$('#cal-410').click(function(){ badger.fetch("410", 47909); });
+	$('#cal-17').click(function(){ badger.fetch("17", 47909); });
+	$('#cal-22').click(function(){ badger.fetch("22", 47909); });
+	$('#cal-223').click(function(){ badger.fetch("223", 47909); });
+	$('#cal-22mag').click(function(){ badger.fetch("22mag", 47909); });
+	$('#cal-3006').click(function(){ badger.fetch("3006", 47909); });
+	$('#cal-3030').click(function(){ badger.fetch("3030", 47909); });
+	$('#cal-308').click(function(){ badger.fetch("308", 47909); });
+	$('#cal-556').click(function(){ badger.fetch("556", 47909); });
+	$('#cal-762').click(function(){ badger.fetch("762", 47909); });
 	
 	
 	/////////////////////////////////////////////////////////////////////////////////
+	
+	
+});
+
+badger = {};
+badger.fetch = function(cal, zip){
+	var snapper = new Snap({
+	  element: document.getElementById('content')
+	});		
+	document.ontouchmove = function(event){ event.preventDefault();}
+	snapper.close();
 	$.ajax({
-		url: "http://brassbadger.honsoworld.com/api/",
-		type: "POST",
+		url: "http://brassbadger.honsoworld.com/api/?cal="+cal+"&zip="+zip,
+		type: "GET",
 		success:function(result){
 			$("#apiResults").html("");
 			result = $.parseJSON(result);
 			for (var i = 0; i < result.data.length; i++) {
-				$("#apiResults").append("<div class='notification-box blue-box'><h4>"+result.data[i]['name']+"</h4><div class='clear'></div><p>"+result.data[i]['address']+"</p></div>");
+				var color = "blue";
+				if(result.data[i]['code'] == "0")
+					color = "red";
+				if(result.data[i]['code'] == "1")
+					color = "yellow";
+				if(result.data[i]['code'] == "2")
+					color = "green";
+				$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.data[i]['name']+"</h4><div class='clear'></div><p>"+result.data[i]['address']+", "+result.data[i]['city']+", "+result.data[i]['state']+" "+result.data[i]['zip']+" | "+result.data[i]['phone']+"</p></div>");
 			}
+			if(result.data.length == 0)
+				$("#apiResults").append("<div class='notification-box blue-box'><h4>- No Results Found -</h4><div class='clear'></div><p></p></div>");
 		}
 	});
-	
-});
-
+}
 	/*
 		var xTouches = event.touches[0].pageX;
 		var yTouches = event.touches[0].pageY;
