878870a7c77b29b014402c1c673b30223f90979f
honsoworld <taylor@honsoworld.com>
2013-11-15 01:09:03 -0500

    update

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -100,7 +100,7 @@ badger.fetch = function(cal, zip){
 					color = "yellow";
 				if(result.data[i]['code'] == "2")
 					color = "green";
-				$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.data[i]['name']+" ("+result.data[i]['status']+")</h4><div class='clear'></div><p>"+result.data[i]['address']+", "+result.data[i]['city']+", "+result.data[i]['state']+" "+result.data[i]['zip']+" | "+result.data[i]['phone']+"</p></div>");
+				$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.data[i]['name']+" ("+result.data[i]['status']+")</h4><div class='clear'></div><p>"+result.data[i]['address']+", "+result.data[i]['city']+", "+result.data[i]['state']+" "+result.data[i]['zip']+" | "+result.data[i]['phone']+"<br />"+result.data[i]['updated']+"</p></div>");
 			}
 			if(result.data.length == 0)
 				$("#apiResults").append("<div class='notification-box blue-box'><h4>- No Results Found -</h4><div class='clear'></div><p></p></div>");
