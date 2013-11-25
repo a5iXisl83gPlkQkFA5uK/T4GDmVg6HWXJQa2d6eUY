5bb5010b7befd693019e5196dc73de1d1033172d
honsoworld <taylor@honsoworld.com>
2013-11-25 12:42:34 -0500

    ip based geo

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -1,10 +1,10 @@
 badger = {};
-
+badger.api = 1;
 badger.zip = 47909;
 badger.saveStoreChecks = function(){
 	var stores = [];
-	$(".nav-item.stores.checked-v2").each(function(){
-		stores.push($(this).data("storeid"));
+	$(".nav-item.stores.checked-v2").each(function(){		var sid = $(this).data("storeid");		if(sid && sid != "undefined"){
+			stores.push(sid);		}
 	})
 	stores = '["'+stores.join('","')+'"]';
 	if(stores == '[""]')
@@ -16,7 +16,7 @@ badger.getZipStores = function(callback){
 	$(".nav-item.stores").remove()
 	$("#nav-stores-text").html("");
 	$.ajax({
-		url: "http://brassbadger.com/api/?r=s&zip="+badger.zip,
+		url: "http://brassbadger.com/api/?api="+badger.api+"&function=store&zip="+badger.zip,
 		type: "GET",
 		success:function(result){
 			result = $.parseJSON(result);
@@ -26,26 +26,28 @@ badger.getZipStores = function(callback){
 				if( savedStoreChecks.length > 0 )
 					usedSavedValues = true;
 			}
-			for (var i = 0; i < result.data.length; i++) {
+			for (var i = 0; i < result.stores.length; i++) {
 				var checkedClass = "";
-				if(usedSavedValues && $.inArray(""+result.data[i]['id']+"", savedStoreChecks) > -1 ){
+				if(usedSavedValues && $.inArray(""+result.stores[i]['id']+"", savedStoreChecks) > -1 ){
 					checkedClass = "checked-v2";
 				}
 				if(!usedSavedValues)
 					checkedClass = "checked-v2";
-				var item = $('<div class="nav-item checker checkbox-v2 '+checkedClass+' stores" style="background-size: 16px 16px; background-position:22px; 3px;padding-left:60px;" data-storeid="'+result.data[i]['id']+'">'+result.data[i]['address']+'</div>');
-				item.click(function(){
-					$(this).toggleClass('checked-v2');
-					badger.updateOverview();
-					badger.saveStoreChecks();
-					$("#subHeader").html("Getting Started");
-					$("#apiResults").html("");
-					$("#apiResults").append(badger.homeContent);
+				if(result.stores[i]['id']){
+					var item = $('<div class="nav-item checker checkbox-v2 '+checkedClass+' stores" style="background-size: 16px 16px; background-position:22px; 3px;padding-left:60px;" data-storeid="'+result.stores[i]['id']+'">'+result.stores[i]['address']+'</div>');
+					item.click(function(){
+						$(this).toggleClass('checked-v2');
+						badger.updateOverviewAjax();
+						badger.saveStoreChecks();
+						$("#subHeader").html("Getting Started");
+						$("#apiResults").html("");
+						$("#apiResults").append(badger.homeContent);
+						
+						return false;
+					})
 					
-					return false;
-				})
-				
-				$("#nav-stores").after(item);
+					$("#nav-stores").after(item);				
+				}
 			}
 			$("#nav-stores-text").html("NEAR " + badger.zip);
 			badger.setHeight();
@@ -57,9 +59,10 @@ badger.getZipStores = function(callback){
 
 badger.getSelectedStores = function(){
 	var stores = "";
-	$(".nav-item.stores.checked-v2").each(function(){
-		stores += "|" + $(this).data("storeid");
+	$(".nav-item.stores.checked-v2").each(function(){		var sid = $(this).data("storeid");		if(sid && sid != "undefined"){
+			stores += "|" + sid;		}
 	})
+	$("#nav-stores-text").html("NEAR " + badger.zip);
 	return stores;
 }
 
@@ -77,32 +80,34 @@ badger.setHeight = function(){
 
 badger.fetch = function(cal){
 	var stores = badger.getSelectedStores();
-	$("#apiResults").html("<p>Loading...</p>");	
+	$("#apiResults").html('<div style="margin-top: 70px;"><img width="32" height="32" alt="img" src="images/loading.gif" style="display: block; margin: auto;"></div>');	
 	badger.snapper.close();
 	$.ajax({
-		url: "http://brassbadger.com/api/?cal="+cal+"&store="+stores,
+		url: "http://brassbadger.com/api/?api="+badger.api+"&function=cal&zip="+badger.zip+"&cal="+cal+"&store="+stores,
 		type: "GET",
 		success:function(result){
 			$("#apiResults").html("");
 			result = $.parseJSON(result);
-			for (var i = 0; i < result.data.length; i++) {
+			badger.zip = result.request.zip;
+			$("#nav-stores-text").html("NEAR " + badger.zip);
+			for (var i = 0; i < result.results.length; i++) {
 				var color = "blue";
-				if(result.data[i]['code'] == "1")
+				if(result.results[i]['code'] == "1")
 					color = "red";
-				if(result.data[i]['code'] == "2")
+				if(result.results[i]['code'] == "2")
 					color = "blue";
-				if(result.data[i]['code'] == "3")
+				if(result.results[i]['code'] == "3")
 					color = "yellow";
-				if(result.data[i]['code'] == "4")
+				if(result.results[i]['code'] == "4")
 					color = "green";
 				var price = "";
-				if(result.data[i]['price'] != "")
-					price = "$"+result.data[i]['price'];
+				if(result.results[i]['price'] != "")
+					price = "$"+result.results[i]['price'];
 					
-				if(result.data[i]['status'] != "Ad"){
-					$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.data[i]['name']+"</h4><div class='clear'></div><p><b>"+price+"</b> "+result.data[i]['status']+" as of "+result.data[i]['updated']+"<br />"+result.data[i]['address']+", "+result.data[i]['city']+", "+result.data[i]['state']+" "+result.data[i]['zip']+"</p></div>");
+				if(result.results[i]['status'] != "Ad"){
+					$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.results[i]['name']+"</h4><div class='clear'></div><p><b>"+price+"</b> "+result.results[i]['status']+" as of "+result.results[i]['updated']+"<br />"+result.results[i]['address']+", "+result.results[i]['city']+", "+result.results[i]['state']+" "+result.results[i]['zip']+"</p></div>");
 				} else {
-					var ad = $("<div class='notification-box "+color+"-box ad'>"+result.data[i]['html']+"</div>");
+					var ad = $("<div class='notification-box "+color+"-box ad'>"+result.results[i]['html']+"</div>");
 					ad.find("a").click(function(e){
 						window.open( $(this).attr('href'), '_system' );
 						e.preventDefault();
@@ -111,54 +116,29 @@ badger.fetch = function(cal){
 				}
 
 			}
-			if(result.data.length == 0)
+			if(result.results.length == 0)
 				$("#apiResults").append("<div class='notification-box blue-box'><h4>- No Results Found -</h4><div class='clear'></div><p></p></div>");
+			badger.updateOverview(result);
 		}
 	});
-	badger.updateOverview();
+	badger.setHeight();
 }
-
-badger.updateOverview = function(){
+badger.updateOverviewAjax = function(){
 	var stores = badger.getSelectedStores();
 	badger.setHeight();
 	$.ajax({
-		url: "http://brassbadger.com/api/?r=o&store="+stores,
+		url: "http://brassbadger.com/api/?api="+badger.api+"&function=overview&zip="+badger.zip+"&store="+stores,
 		type: "GET",
 		success:function(result){
 			result = $.parseJSON(result);
-			$(".nav-item.cal")
-				.removeClass("type-blue")
-				.removeClass("type-grey")
-				.removeClass("type-red")
-				.removeClass("type-yellow")
-				.removeClass("type-green")
-				.addClass("type-grey");
-			for (var i = 0; i < result.data.length; i++) {
-				var color = "grey";
-				if(result.data[i]['code'] == "1")
-					color = "red";
-				if(result.data[i]['code'] == "2")
-					color = "blue";
-				if(result.data[i]['code'] == "3")
-					color = "yellow";
-				if(result.data[i]['code'] == "4")
-					color = "green";
-				$("#cal-" + result.data[i]['cal'])
-					.removeClass("type-blue")
-					.removeClass("type-grey")
-					.removeClass("type-red")
-					.removeClass("type-yellow")
-					.removeClass("type-green")
-					.addClass("type-" + color);
-			}
+			badger.updateOverview(result);
+			badger.zip = result.request.zip;
+			$("#nav-stores-text").html("NEAR " + badger.zip);
 			badger.setHeight();
 		}
 	});
 }
-
-badger.geoLocateCallback = function(json){
-	badger.zip = json.postalCodes[0].postalCode;
-	window.localStorage.setItem( 'zipcode', badger.zip);
+badger.updateOverview = function(result){
 	$(".nav-item.cal")
 		.removeClass("type-blue")
 		.removeClass("type-grey")
@@ -166,28 +146,26 @@ badger.geoLocateCallback = function(json){
 		.removeClass("type-yellow")
 		.removeClass("type-green")
 		.addClass("type-grey");
-	badger.getZipStores(function(){
-		badger.updateOverview();
-	});
-	alert("GeoLocate: Using " + badger.zip + " as zipcode.");
+	for (var i = 0; i < result.overview.length; i++) {
+		var color = "grey";
+		if(result.overview[i]['code'] == "1")
+			color = "red";
+		if(result.overview[i]['code'] == "2")
+			color = "blue";
+		if(result.overview[i]['code'] == "3")
+			color = "yellow";
+		if(result.overview[i]['code'] == "4")
+			color = "green";
+		$("#cal-" + result.overview[i]['cal'])
+			.removeClass("type-blue")
+			.removeClass("type-grey")
+			.removeClass("type-red")
+			.removeClass("type-yellow")
+			.removeClass("type-green")
+			.addClass("type-" + color);
+	}
 }
-badger.geoLocate = function(){
-	navigator.geolocation.getCurrentPosition(function(pos){
-		alert(pos.coords.latitude);
-		var script = document.createElement("script");
-		script.src = "http://ws.geonames.org/findNearbyPostalCodesJSON?lat=" + pos.coords.latitude + "&lng=" + pos.coords.longitude + "&callback=badger.geoLocateCallback";
-		document.getElementsByTagName("head")[0].appendChild(script);
-		alert("cool");
-	}, function(){
-		alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
 
-		console.log(arguments);
-	},{
-         enableHighAccuracy: true
-              ,timeout : 15000
-    });
-}
 badger.homeContent = $("<div style='padding: 10px;'><h1>What is BrassBadger?</h1><p>BrassBadger attempts to keep track of the ammunition  availability of a large discount department store chain. Please understand that keeping track of inventory is complicated and that the ammo availability shown from this app may not be accurate. Remember to always be respectful to all store employees, associates, and shoppers.</p><h2>How do I specify my location?</h2><p>The last two items on the menu allow you to explicitly specify your zip code or attempt to use geolocating to estimate your zip code.</p><h2>How do I pick what stores I&rsquo;m interested in?</h2><p>Once you have specified a location, the last section of the menu will be populated with stores near you. By default, ammo availability will include results from all stores near the specified zip code. You can exclude stores by <em>unchecking</em> them in the menu.</p><h2>What do the colors  and statuses mean?</h2><p><span style='color:#629b56;'><strong>Green (In stock</strong>)<br /></span>Chances are good that there are three or more products available.</p><p> <span style='color:#b0b53a;'><strong>Yellow (Limited stock</strong>)<br /></span>There are probably only a couple products available.</p><p> <span style='color:#606bb2;'><strong>Blue (Availability unknown</strong>)<br /></span>More specific availability details are unavailable, however the data source is not explicitly reporting the product as &ldquo;Out of stock&rdquo;. Empirical evidence would suggest that there is a fair chance that some of the product is actually available.</p><p> <span style='color:#a9474a;'><strong>Red (Out of stock</strong>)<br /></span>It is unlikely for that the product to be available. </p><p> <span style='color:#a9474a;'><strong>Red (Expired</strong>)<br /></span>At some point in the last day or so the data source reported the status of the product. However, several subsequent inquiries failed to maintain the availability details for this product. This could happen after a product that is not  typically carried by a store goes out of stock.</p><p> <span style='color:#c4c4c4;'><strong>Grey</strong><br /></span>No information is available.</p><h2>What does the date/time mean?</h2><p>The date/time information associated with each availability tile refers to the EST time that the data source was last <em>checked</em> by our servers. Please be aware that this does not reflect the date/time that the availability was last <em>updated</em> in the data source.</p><h2>Why are there availability discrepancies?</h2><p> Our accuracy is dependent on the data sources we pull from.  BrassBadger is in no way affiliated with any discount department store chain or  third-party source of information.</p><h2>Why isn&rsquo;t geolocating  working?</h2><p> In order to take advantage of geolocating, your device must  support geolocating/location services. If applicable, your GPS should be on and  proper permissions enabled. Poor GPS reception can also prevent geolocating  from working properly.</p></div>");
 	/*
 		var xTouches = event.touches[0].pageX;
@@ -286,25 +264,28 @@ $(document).ready(function(){
 	/////////////////////////////////////////////////////////////////////////////////
 	
 	badger.getZipStores(function(){
-		badger.updateOverview();
+		badger.updateOverviewAjax();
 		try {
 			navigator.splashscreen.hide();
 		} catch(err){}
 	});
 	$(".deploy-sidebar").click();
-	$("#nav-zip").click(function(){
-		badger.zip = prompt("Zipcode:",badger.zip);
-		window.localStorage.setItem( 'zipcode', badger.zip);
-		$(".nav-item.cal")
-			.removeClass("type-blue")
-			.removeClass("type-grey")
-			.removeClass("type-red")
-			.removeClass("type-yellow")
-			.removeClass("type-green")
-			.addClass("type-grey");
-		badger.getZipStores(function(){
-			badger.updateOverview();
-		});
+	$("#nav-zip").click(function(){	
+		var newZip = prompt("Zipcode:",badger.zip);
+		if(newZip){
+			badger.zip = newZip;
+			window.localStorage.setItem( 'zipcode', badger.zip);
+			$(".nav-item.cal")
+				.removeClass("type-blue")
+				.removeClass("type-grey")
+				.removeClass("type-red")
+				.removeClass("type-yellow")
+				.removeClass("type-green")
+				.addClass("type-grey");
+			badger.getZipStores(function(){
+				badger.updateOverviewAjax();
+			});
+		}
 	});
 	$(".deploy-home").click(function(){
 		$("#subHeader").html("Getting Started");
@@ -314,21 +295,10 @@ $(document).ready(function(){
 	
 	
 	$("#nav-geo").click(function(){
-		if (confirm('This will use your device\'s geolocation service to find your zipcode. Make sure your location services are enabled. Do you want to continue?')){
-			navigator.geolocation.getCurrentPosition(function(pos){
-				var script = document.createElement("script");
-				script.src = "http://ws.geonames.org/findNearbyPostalCodesJSON?lat=" + pos.coords.latitude + "&lng=" + pos.coords.longitude + "&callback=badger.geoLocateCallback";
-				document.getElementsByTagName("head")[0].appendChild(script);
-			}, function(){
-				alert('code: '    + error.code    + '\n' +
-					  'message: ' + error.message + '\n');
-
-				console.log(arguments);
-			},{
-				 enableHighAccuracy: true
-					  ,timeout : 15000
-			});
-		}
+		badger.zip = "";
+		badger.getZipStores(function(){
+			badger.updateOverviewAjax();
+		});
 	});
 	
 		
