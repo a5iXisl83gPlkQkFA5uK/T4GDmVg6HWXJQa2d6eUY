c0a8196db7bf3969d23df00b639af415ecaee249
honsoworld <taylor@honsoworld.com>
2013-12-05 19:00:01 -0500

    .

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -656,42 +656,78 @@ $(document).ready(function(){
 								if(raw && raw.length > 0 && raw[0]){
 									nice.name = raw[0]['item']['signingDesc'];
 								}
-								for(var m in raw[0]['stores']){
-									var code = 5;
-									var status = raw[0]['stores'][m]['stockStatus'];
-									if(raw[0]['stores'][m]['stockStatus'] == "In stock"){
-										code = 4;
+								var isAmmo = false;
+								if( 
+									nice.name.indexOf("357") !== -1 || nice.name.indexOf("38") !== -1 || nice.name.indexOf("380") !== -1 || 
+									nice.name.indexOf("40") !== -1 || nice.name.indexOf("44") !== -1 || nice.name.indexOf("45") !== -1 || 
+									nice.name.indexOf("9") !== -1 || nice.name.indexOf("22") !== -1 || nice.name.indexOf("223") !== -1 || 
+									nice.name.indexOf("243") !== -1 || nice.name.indexOf("30") !== -1 || nice.name.indexOf("06") !== -1 || 
+									nice.name.indexOf("308") !== -1 || nice.name.indexOf("5.56") !== -1 || nice.name.indexOf("7.62") !== -1 || 
+									nice.name.indexOf("17") !== -1 || nice.name.indexOf("12") !== -1 || nice.name.indexOf("20") !== -1 || 
+									nice.name.indexOf("410") !== -1 || nice.name.indexOf("FMJ") !== -1 || nice.name.indexOf("AMMO") !== -1 || 
+									nice.name.indexOf("AUTO") !== -1 || nice.name.indexOf("GRAIN") !== -1 || nice.name.indexOf("GR") !== -1 || 
+									nice.name.indexOf("RNDS") !== -1 || nice.name.indexOf("FED") !== -1 || nice.name.indexOf("TULA") !== -1 || 
+									nice.name.indexOf("JSP") !== -1 || nice.name.indexOf("MAG") !== -1 || nice.name.indexOf("JHP") !== -1 || 
+									nice.name.indexOf("REM") !== -1 || nice.name.indexOf("UMC") !== -1 || nice.name.indexOf("SPL") !== -1 || 
+									nice.name.indexOf("BRASS") !== -1 || nice.name.indexOf("S&W") !== -1 || nice.name.indexOf("PAK") !== -1 || 
+									nice.name.indexOf("50") !== -1 || nice.name.indexOf("100") !== -1 || nice.name.indexOf("MC") !== -1 || 
+									nice.name.indexOf("COLT") !== -1 || nice.name.indexOf("AMO") !== -1 || nice.name.indexOf("LEAD") !== -1 || 
+									nice.name.indexOf("BOX") !== -1 || nice.name.indexOf("LUGER") !== -1 || nice.name.indexOf("SHO") !== -1 || 
+									nice.name.indexOf("PTD") !== -1 || nice.name.indexOf("SP") !== -1 || nice.name.indexOf("CAL") !== -1 || 
+									nice.name.indexOf("FMC") !== -1 || nice.name.indexOf("BX") !== -1 || nice.name.indexOf("MATCH") !== -1 || 
+									nice.name.indexOf("VARM") !== -1 || nice.name.indexOf("MSR") !== -1 || nice.name.indexOf("FUSI") !== -1 || 
+									nice.name.indexOf("POINT") !== -1 || nice.name.indexOf("SUPER") !== -1 || nice.name.indexOf("BALL") !== -1 || 
+									nice.name.indexOf("HMR") !== -1 || nice.name.indexOf("PREM") !== -1 || nice.name.indexOf("CHP") !== -1 || 
+									nice.name.indexOf("LR") !== -1 || nice.name.indexOf("SOLID") !== -1 || nice.name.indexOf("VELOCITY") !== -1 || 
+									nice.name.indexOf("RIFLE") !== -1 || nice.name.indexOf("NOSE") !== -1 || nice.name.indexOf("QUIET") !== -1 || 
+									nice.name.indexOf("PLATED") !== -1 || nice.name.indexOf("CCI") !== -1 || nice.name.indexOf("SLUG") !== -1 || 
+									nice.name.indexOf("TARGET") !== -1 || nice.name.indexOf("LOAD") !== -1 || nice.name.indexOf("GA") !== -1 || 
+									nice.name.indexOf("16") !== -1 || nice.name.indexOf("STS") !== -1 || nice.name.indexOf("BUCK") !== -1 || 
+									nice.name.indexOf("SHOT") !== -1 || nice.name.indexOf("USA") !== -1 || nice.name.indexOf("ARM") !== -1 || 
+									nice.name.indexOf("SABOT") !== -1 || nice.name.indexOf("SPRG") !== -1 || nice.name.indexOf("ACP") !== -1 || 
+									nice.name.indexOf("BROWNING") !== -1 || nice.name.indexOf("WESSON") !== -1 || nice.name.indexOf("45") !== -1 
+								) {
+									isAmmo = true;
+									for(var m in raw[0]['stores']){
+										var code = 5;
+										var status = raw[0]['stores'][m]['stockStatus'];
+										if(raw[0]['stores'][m]['stockStatus'] == "In stock"){
+											code = 4;
+										}
+										if(raw[0]['stores'][m]['stockStatus'] == "Availability unknown"){
+											code = 3;
+											status = "In stock";
+										}
+										if(raw[0]['stores'][m]['stockStatus'] == "Limited stock"){
+											code = 2;
+										}
+										if(raw[0]['stores'][m]['stockStatus'] == "Out of stock"){
+											code = 1;
+										}
+										nice.results[m] = {
+											"address" : raw[0]['stores'][m]['address']['street1'],
+											"cal" : "",
+											"city" : raw[0]['stores'][m]['address']['city'],
+											"code" : code,
+											"name" : raw[0]['item']['signingDesc'],
+											"phone" : "("+raw[0]['stores'][m]['phone']['areaCode']+") "+raw[0]['stores'][m]['phone']['prefix']+"-"+raw[0]['stores'][m]['phone']['suffix'],
+											"price" : raw[0]['stores'][m]['price'],
+											"state" : raw[0]['stores'][m]['address']['state']['code'],
+											"status" : status,
+											"upc" : result.text,
+											"updated" : "just now",
+											"zip" : raw[0]['stores'][m]['address']['zip']['code']
+										};
 									}
-									if(raw[0]['stores'][m]['stockStatus'] == "Availability unknown"){
-										code = 3;
-										status = "In stock";
-									}
-									if(raw[0]['stores'][m]['stockStatus'] == "Limited stock"){
-										code = 2;
-									}
-									if(raw[0]['stores'][m]['stockStatus'] == "Out of stock"){
-										code = 1;
-									}
-									nice.results[m] = {
-										"address" : raw[0]['stores'][m]['address']['street1'],
-										"cal" : "",
-										"city" : raw[0]['stores'][m]['address']['city'],
-										"code" : code,
-										"name" : raw[0]['item']['signingDesc'],
-										"phone" : "("+raw[0]['stores'][m]['phone']['areaCode']+") "+raw[0]['stores'][m]['phone']['prefix']+"-"+raw[0]['stores'][m]['phone']['suffix'],
-										"price" : raw[0]['stores'][m]['price'],
-										"state" : raw[0]['stores'][m]['address']['state']['code'],
-										"status" : status,
-										"upc" : result.text,
-										"updated" : "just now",
-										"zip" : raw[0]['stores'][m]['address']['zip']['code']
-									};
+								} else {
+									badger.showError("blue", "Not ammo...", "This doesn’t look like ammo. ");
 								}
+
 								
 								badger.buildRes(nice);
 								badger.onResize();
 								var ajaxPromise = $.ajax({
-									url: "http://brassbadger.com/api/?api="+badger.api+"&function=upcScan&upc="+upc+"&name="+nice.name,
+									url: "http://brassbadger.com/api/?api="+badger.api+"&function=upcScan&upc="+upc+"&name="+nice.name+"&isAmmo="+isAmmo,
 									method: "GET",
 									async: true,
 									success: function(g){return;},
@@ -699,21 +735,22 @@ $(document).ready(function(){
 								});
 							},
 							error: function(jqXHR, textStatus, errorThrown){
-								alert("Error looking up product.");
+								badger.showError("blue", "Error looking up product", "No information for this UPC is available for any of the stores you have selected.");
 								//errorCallback(textStatus, errorThrown);
 								badger.onResize();
 							}
 						});
 						
 					} else {
-						alert("Only UPC-A codes are supported");
+						badger.showError("blue", "Invalid barcode", "Only UPC-A codes are supported");
 					}
 				} else {
 					// Cancelled
 				}
 			}, 
 			function (error) {
-				alert("Scanning failed (" + error + ")");
+				badger.showError("blue", "Error", "Scanning failed (" + error + ")");
+
 			}
 		);
 	});
