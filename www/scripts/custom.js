d27ca7399400c237a55b1559c8a40b7344a1193d
honsoworld <taylor@honsoworld.com>
2013-12-06 15:38:00 -0500

    .

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -363,7 +363,7 @@ badger.buildRes = function(result){
 			was = " (was "+result.results[i]['previously']+")";
 		}
 		if(result.results[i]['status'] != "Ad"){
-			$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.results[i]['name']+"</h4><div class='clear'></div><p><b>"+price+"</b> "+result.results[i]['status']+" for the past "+result.results[i]['since']+""+was+"<br /> Last checked "+result.results[i]['updated']+"<br />"+result.results[i]['address']+", "+result.results[i]['city']+", "+result.results[i]['state']+" "+result.results[i]['zip']+"<br />"+result.results[i]['phone']+"&nbsp UPC: "+result.results[i]['upc']+"</p></div>");
+			$("#apiResults").append("<div class='notification-box "+color+"-box'><h4>"+result.results[i]['name']+"</h4><div class='clear'></div><p>"+result.results[i]['status']+" for the past "+result.results[i]['since']+""+was+"<br /><b>"+price+"</b> Last checked "+result.results[i]['updated']+"<br />"+result.results[i]['address']+", "+result.results[i]['city']+", "+result.results[i]['state']+" "+result.results[i]['zip']+"<br />"+result.results[i]['phone']+"&nbsp UPC: "+result.results[i]['upc']+"</p></div>");
 		} else {
 			var ad = $("<div class='notification-box "+color+"-box ad'>"+result.results[i]['html']+"</div>");
 			ad.find("a").click(function(e){
